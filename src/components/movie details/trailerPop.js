8469d072d99bef21e4a6c531235dd403b2ba0fd9
helal0321 <159494281+helal0321@users.noreply.github.com>
2024-06-26 03:42:19 +0300

    Update trailerPop.js

--- src/components/movie details/trailerPop.js
+++ src/components/movie details/trailerPop.js
@@ -1,20 +1,16 @@
 import '../css/trailerPop.css'
-import { useDispatch,useSelector } from 'react-redux'
-import {reset} from '../../rtk/trailerslice'
-function TrailerPop(){
-    let Url=useSelector((state)=>{return state.trailer.url})
-    let dispatch=useDispatch()
+function TrailerPop({url,closepopup}){
     return(
         <div className="popup">
 
                 <div className='popop_container'>
                     <button onClick={()=>{
-                         dispatch(reset())
+                         closepopup()
                      }}>x</button>
                     <iframe
                     title="YouTube Video Player"
 
-                    src={`https://www.youtube.com/embed/${Url}`}
+                    src={`https://www.youtube.com/embed/${url}`}
                     frameBorder="0"
                     allowFullScreen
                     ></iframe>
